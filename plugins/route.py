8ab8047b3851e398ca0645be76ee8cf5dacf6781
Thelastone <142857872+itskissu@users.noreply.github.com>
2025-07-22 10:47:29 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response(" yes your boy deployed join us - @kissuxbots on telegram")
+    return web.json_response("Bot Deployed join us - @kissuxbots on telegram")
